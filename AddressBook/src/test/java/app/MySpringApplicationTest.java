c5604abdd2258597645821253e8aa0fc4d6849bc
Jacky Chiu <jackyzhanchiu@gmail.com>
2020-02-07 15:35:40 -0500

    full integration test

--- AddressBook/src/test/java/app/MySpringApplicationTest.java
+++ AddressBook/src/test/java/app/MySpringApplicationTest.java
@@ -8,10 +8,11 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.not;
+
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -20,10 +21,34 @@ class MySpringApplicationTest {
     private MockMvc mockMvc;
 
     @Test
-    public void shouldReturnDefaultMessage() throws Exception {
+    public void fullSystemTest() throws Exception {
         this.mockMvc.perform(post("/newAddressBook")).andExpect(status().isOk());
+
         this.mockMvc.perform(get("/addressBook").param("id", "1"))
-                .andDo(print()).andExpect(status().isOk())
-                .andExpect(content().string(containsString("AddressBook")));
+                .andExpect(content().string(containsString("No buddies")));
+
+        this.mockMvc.perform(post("/newBuddyInfo")
+                .param("addressBookId", "1")
+                .param("name", "buddy1")
+                .param("number", "613")
+        ).andExpect(status().isOk());
+
+        this.mockMvc.perform(post("/newBuddyInfo")
+                .param("addressBookId", "1")
+                .param("name", "buddy2")
+                .param("number", "416")
+        ).andExpect(status().isOk());
+
+        this.mockMvc.perform(get("/addressBook").param("id", "1"))
+                .andExpect(content().string(containsString("buddy1")))
+                .andExpect(content().string(containsString("buddy2")));
+
+        this.mockMvc.perform(post("/deleteBuddyInfo")
+                .param("id", "2"))
+                .andExpect(status().isOk());
+
+        this.mockMvc.perform(get("/addressBook").param("id", "1"))
+                .andExpect(content().string(not(containsString("buddy1"))))
+                .andExpect(content().string(containsString("buddy2")));
     }
 }
